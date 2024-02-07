e12a847a7a4235a89d8316db20178c8300c62744
Enis <en.simsir@gmail.com>
2024-02-08 01:42:43 +0300

    h

---
+++ Assets/Scripts/Game Manager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<GameManager>();
+                if (_instance == null)
+                {
+                    GameObject singletonObject = new GameObject(typeof(GameManager).Name);
+                    _instance = singletonObject.AddComponent<GameManager>();
+                    DontDestroyOnLoad(singletonObject);
+                }
+            }
+            return _instance;
+        }
+    }
+
+    // Diğer GameManager fonksiyonları buraya eklenebilir
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Oyun yönetimiyle ilgili diğer metotlar buraya eklenebilir
+}
+
