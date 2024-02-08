a89dc84a32e0627f850b35ddc536a7242115017c
Enis <en.simsir@gmail.com>
2024-02-08 18:36:31 +0300

    buton bölüm yüklüyor

---
+++ Assets/Scripts/Game Controller.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    private static GameController _instance;
+
+    public static GameController Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<GameController>();
+                if (_instance == null)
+                {
+                    GameObject singletonObject = new GameObject(typeof(GameController).Name);
+                    _instance = singletonObject.AddComponent<GameController>();
+                    DontDestroyOnLoad(singletonObject);
+                }
+            }
+            return _instance;
+        }
+    }
+
+    // Diğer GameController fonksiyonları buraya eklenebilir
+
+    // Yeni sahneyi yüklemek için metot
+    public void LoadNewScene()
+    {
+        Debug.Log("oeooe");
+        int currentLevel;
+        if (GameModel.Instance != null)
+        {
+            currentLevel = GameModel.Instance.currentLevel;
+            SceneManager.LoadScene(currentLevel);
+        }
+
+        
+    }
+}
---
+++ Assets/Scripts/UI/Level Button Controller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelButtonController : MonoBehaviour
+{
+    public Text levelText; // Seviye bilgisini göstermek için bir Text bileşeni
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Seviye bilgisini güncelleyen metot
+    public void UpdateLevelText(int level)
+    {
+        if (levelText != null)
+        {
+            levelText.text = "Level: " + level.ToString();
+        }
+    }
+
+}
